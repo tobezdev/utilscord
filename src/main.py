fd9ba3a67bca4ed3b0b17356b5d9a73eaec696c2
tobezdev <tobester28@icloud.com>
2026-09-07 21:40:46 +0100

    feat: add main bot loading script

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+from discord import Bot, Intents, InteractionContextType, IntegrationType
+
+
+def main() -> None:
+    i = Intents.default()
+    i.message_content = True
+    bot = Bot(
+        intents=i,
+        owner_id=969254887621820526,
+        auto_sync_commands=True,
+        default_command_contexts={
+            InteractionContextType.guild, 
+            InteractionContextType.private_channel
+        },
+        default_command_integration_types={
+            IntegrationType.guild_install
+        }
+    )
+
+    cogs_directory = Path(__file__).resolve().parent / "cogs"
+    for path in sorted(cogs_directory.glob("*.py")):
+        if not path.name.startswith("_"):
+            bot.load_extension(f"cogs.{path.stem}")
+
+    load_dotenv()
+    token = os.getenv("TOKEN")
+    if token:
+        bot.run(token)
+
+
+if __name__ == "__main__":
+    main()
