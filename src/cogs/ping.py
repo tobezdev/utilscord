825c2233361370a8bcedf3e10ca9db6bf73d3bf8
tobezdev <tobester28@icloud.com>
2026-09-07 21:52:19 +0100

    feat(ping): add ping command

---
+++ src/cogs/ping.py
@@ -0,0 +1,21 @@
+from discord import Bot, ApplicationContext
+from discord.ext.commands import Cog, cooldown, BucketType
+from discord.commands import slash_command
+from discord.ui import DesignerView, Container, TextDisplay
+
+
+class Ping(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @slash_command(name="ping", description="Check the bot's latency.")
+    @cooldown(1, 300, BucketType.user)
+    @cooldown(1, 5, BucketType.default)
+    async def ping(self, ctx: ApplicationContext):
+        latency = round(self.bot.latency * 1000, None)
+        container = Container(TextDisplay(f"### 🏓 Pong!\nLatency: {latency}ms"))
+        return await ctx.respond(view=DesignerView(container))
+
+
+def setup(bot: Bot):
+    bot.add_cog(Ping(bot))
